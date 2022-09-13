037a01668d42f08482442aa307cf34266d8fb5b7
Nisha <nishapoonia010@gmail.com>
2022-09-13 20:56:15 +0530

    second commit

--- sms/settings.py
+++ sms/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from django import os
+import os
 import django_heroku
 import dj_database_url
 
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'))
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 
 # Default primary key field type
