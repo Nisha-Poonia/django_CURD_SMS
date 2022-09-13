fbb855a6620a679c38c2e05908edb334bc292d57
Nisha <nishapoonia010@gmail.com>
2022-09-13 23:04:45 +0530

    updated code

--- sms/settings.py
+++ sms/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-tyrp$ne$7hndsvyw+-iv7(^s(shl)sh5ipqm(06n$zqy9f^&&)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studentappcurd.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,9 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'))
+
+django_heroku.settings(locals())
